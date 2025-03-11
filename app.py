19c5df181ecd8e4032b813e0e6826836bbf513db
waqas1343 <waqasilyas5904@gmail.com>
2025-03-12 02:32:31 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import pandas as pd
+from io import BytesIO
+
+# Streamlit app configuration
+st.set_page_config(page_title="File Converter", layout="centered")
+st.markdown("<h1 style='text-align: center; color: red;'>💻 Code With Irsa</h1>",unsafe_allow_html=True)
+st.markdown(
+    "<h1 style='text-align: center; font-size: 2.5rem;color:red'>⌕ File Converter & Cleaner ⌕</h1>",
+    unsafe_allow_html=True
+)
+st.write("Upload CSV or Excel files, clean data, and convert formats.")
+
+# File uploader
+files = st.file_uploader("Upload CSV or Excel Files.", type=["csv", "xlsx"], accept_multiple_files=True)
+
+if files:
+    for file in files:
+        ext = file.name.split(".")[-1]
+        df = pd.read_csv(file) if ext == "csv" else pd.read_excel(file)
+
+        st.subheader(f"{file.name} - Preview")
+        st.dataframe(df.head())
+
+        # Remove duplicates
+        if st.checkbox(f"Remove Duplicates - {file.name}"):
+            df = df.drop_duplicates()
+            st.success("Duplicates Removed")
+            st.dataframe(df.head())
+
+        # Fill missing values
+        if st.checkbox(f"Fill Missing Values - {file.name}"):
+            df.fillna(df.select_dtypes(include=["number"]).mean(), inplace=True)
+            st.success("Missing values filled with mean")
+            st.dataframe(df.head())
+
+        # Select specific columns
+        selected_columns = st.multiselect(
+            f"Select Columns - {file.name}", df.columns.tolist(), default=df.columns.tolist()
+        )
+        df = df[selected_columns]
+        st.dataframe(df.head())
+
+        # Show chart if numeric columns exist
+        if st.checkbox(f"Show Chart - {file.name}") and not df.select_dtypes(include="number").empty:
+            st.bar_chart(df.select_dtypes(include="number").iloc[:, :2])
+
+        # Convert file format
+        format_choice = st.radio(f"Convert {file.name} to:", ["csv", "Excel"], key=file.name)
+
+        if st.button(f"Download {file.name} as {format_choice}"):
+            output = BytesIO()
+            if format_choice == "csv":
+                df.to_csv(output, index=False)
+                mime = "text/csv"
+                new_name = file.name.replace(ext, "csv")
+            else:
+                df.to_excel(output, index=False, engine='openpyxl')
+                mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                new_name = file.name.replace(ext, "xlsx")
+
+            output.seek(0)
+            st.download_button(label="Download file", data=output.getvalue(), file_name=new_name, mime=mime)
+
+        st.success("Processing Complete!")
